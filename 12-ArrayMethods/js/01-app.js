dac5d10714bc4966946105f3f6649043d4f02ed5
Mauricio Pucheta <mauriciopucheta21@gmail.com>
2025-09-26 19:13:40 -0300

    add: Se agrega método de ArrayMethods llamados includes y some.

--- 12-ArrayMethods/js/01-app.js
+++ 12-ArrayMethods/js/01-app.js
@@ -7,4 +7,17 @@ const carrito = [
     { nombre: 'Audifonos', precio: 300 },
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
-]
+];
+
+// Esto funcionará para el array de meses
+const resultado = meses.includes('Enero');
+console.log(resultado);
+
+// Esto funcionará para el array de carritos
+const resultado2 = carrito.some( producto => {
+    return producto.nombre === 'Celular'; 
+});
+
+console.log(resultado2);
+
+// Aclaraciones: La diferencia entre includes y some, es que includes solamente sirve para arrays de índices mientras que some, sirve para arrays de índices y array de objetos.
