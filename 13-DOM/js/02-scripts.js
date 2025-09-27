5a6c09df84f13ce9359a3a7640f4e91f620690d5
Mauricio Pucheta <mauriciopucheta21@gmail.com>
2025-09-26 22:01:52 -0300

    add: Se agrega contenido relacionado a 'getElementByClassName'.

---
+++ 13-DOM/js/02-scripts.js
@@ -0,0 +1,14 @@
+// Seleccionamos un elemento
+const header = document.getElementsByClassName('header');
+console.log(header);
+
+const hero = document.getElementsByClassName('hero');
+console.log(hero);
+
+// Si las clases existen más de una vez
+const contenedores = document.getElementsByClassName('contenedor');
+console.log(contenedores);
+
+// Si la clase no existe
+const noExiste = document.getElementsByClassName('no-existe');
+console.log(noExiste);
